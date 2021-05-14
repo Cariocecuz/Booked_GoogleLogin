5bd53600576c1e1cea1a7ac6bbab748097f973b6
Filipe Velosa <33222172+Cariocecuz@users.noreply.github.com>
2021-05-14 12:35:25 +0100

    Create ExternalAuthLoginPresenter.php

---
+++ Presenters/Authentication/ExternalAuthLoginPresenter.php
@@ -0,0 +1,108 @@
+<?php
+
+use Google\Service\Resource;
+
+require_once(ROOT_DIR . 'Presenters/Authentication/LoginRedirector.php');
+
+class ExternalAuthLoginPresenter
+{
+    /**
+     * @var ExternalAuthLoginPage
+     */
+    private $page;
+    /**
+     * @var IWebAuthentication
+     */
+    private $authentication;
+    /**
+     * @var IRegistration
+     */
+    private $registration;
+
+    public function __construct(ExternalAuthLoginPage $page, IWebAuthentication $authentication, IRegistration $registration)
+    {
+        $this->page = $page;
+        $this->authentication = $authentication;
+        $this->registration = $registration;
+    }
+
+    public function PageLoad()
+    {
+        if ($this->page->GetType() == 'google') {
+            $this->ProcessGoogleSingleSignOn();
+        }
+        if ($this->page->GetType() == 'fb') {
+            $this->ProcessSocialSingleSignOn('fbprofile.php');
+        }
+    }
+
+    private function ProcessSocialSingleSignOn($page)
+    {
+        $code = $_GET['code'];
+        Log::Debug('Logging in with social. Code=%s', $code);
+        $result = file_get_contents("http://www.social.twinkletoessoftware.com/$page?code=$code");
+        $profile = json_decode($result);
+    
+        $requiredDomainValidator = new RequiredEmailDomainValidator($profile->email);
+        $requiredDomainValidator->Validate();
+        if (!$requiredDomainValidator->IsValid()) {
+            Log::Debug('Social login with invalid domain. %s', $profile->email);
+            $this->page->ShowError(array(Resources::GetInstance()->GetString('InvalidEmailDomain')));
+            return;
+        }
+
+        Log::Debug('Social login successful. Email=%s', $profile->email);
+        $this->registration->Synchronize(new AuthenticatedUser($profile->email,
+            $profile->email,
+            $profile->first_name,
+            $profile->last_name,
+            Password::GenerateRandom(),
+            Resources::GetInstance()->CurrentLanguage,
+            Configuration::Instance()->GetDefaultTimezone(),
+            null,
+            null,
+            null),
+            false,
+            false);
+
+        $this->authentication->Login($profile->email, new WebLoginContext(new LoginData()));
+        LoginRedirector::Redirect($this->page);
+    }
+
+    private function ProcessGoogleSingleSignOn()
+    {
+        
+        $email      = $_SESSION['email'];
+        $firstName  = $_SESSION['givenName'];
+        $lastName   = $_SESSION['familyName'];
+
+        $code = $_GET['code'];
+        Log::Debug('Logging in with google. Code=%s', $code);
+
+        $requiredDomainValidator = new RequiredEmailDomainValidator($email);
+        $requiredDomainValidator->Validate();
+        if (!$requiredDomainValidator->IsValid()) {
+            Log::Debug('Social login with invalid domain. %s', $email);
+            $this->page->ShowError(array(Resources::GetInstance()->GetString('InvalidEmailDomain')));
+            return;
+        }
+
+        Log::Debug('Social login successful. Email=%s', $email);
+        $this->registration->Synchronize(new AuthenticatedUser($email,
+            $email,
+            $firstName,
+            $lastName,
+            Password::GenerateRandom(),
+            Resources::GetInstance()->CurrentLanguage,
+            Configuration::Instance()->GetDefaultTimezone(),
+            null,
+            null,
+            null),
+            false,
+            false);
+
+        $this->authentication->Login($email, new WebLoginContext(new LoginData()));
+        LoginRedirector::Redirect($this->page);
+    }
+
+}
