224d92d9d58fb9f6d5b0f9225744c025d4548e46
Filipe Velosa <33222172+Cariocecuz@users.noreply.github.com>
2021-05-14 12:33:11 +0100

    Update googleConfig.php

--- WebServices/Google/googleConfig.php
+++ WebServices/Google/googleConfig.php
@@ -11,10 +11,10 @@ The credentials must then be inserted below
 The redirect URI path has to be absolute and pointing to /googleAuth.php in
 your instalation.*/
 
-//Google Services Constants
-define("CLIENT_ID","114579221582-9j9t6kalv69qbombgkc0bmgo5naf62p9.apps.googleusercontent.com");
+//Google Services Constants Insert you ID and Secret
+define("CLIENT_ID","");
 //Google Client secret 
-define("CLIENT_SECRET","tq6HYA49B8p_BndUdFZ5Tf0n"); 
+define("CLIENT_SECRET",""); 
 //Google Redirect URI, the path needs to be absolute pointing to /googleAuth.php
 define("REDIRECT_URI", "http://localhost/BookedScheduler-develop/Web/googleAuth.php" );
 
